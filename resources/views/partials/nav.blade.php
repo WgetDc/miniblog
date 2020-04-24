f7a4595ea43be3407541898ff7f478400cbad291
Cristobalius <ctobal.torres@gmail.com>
2020-04-23 23:25:08 -0400

    test

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,6 +1,10 @@
 <nav class="navbar navbar-expand-md bg-white shadow-sm">
-    <a class="navbar-brand" href="{{route('home')}}"></a>
+
+<a class="navbar-brand" href="{{route('home')}}">
+
     {{config('app.name')}}
+
+    </a>
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
         <span class="navbar-toggler-icon"></span>
