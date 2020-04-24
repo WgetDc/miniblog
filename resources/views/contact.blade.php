497e0c0896a188e4b8ee9c611905cf9252a368fc
Cristobalius <ctobal.torres@gmail.com>
2020-04-24 00:37:57 -0400

    migrationes

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,28 +6,32 @@
 
 
 @section('content')
+
+<div class="container">
+
     <h1>Contacto</h1>
 
 
 @include('partials.session-status')
     
-<form method="POST" action="{{route('contact')}}">
+<form class="bg-white shadow rounded py-3 px-4" method="POST" action="{{route('contact')}}">
     @csrf
-
-        <input name="name" placeholder="Nombre..." value="{{old('name')}}">
+        <div class="form-group">
+            <label for="name">Nombre</label>
+        <input class="form-control bg-light shadow-sm border-0" name="name" placeholder="Nombre..." value="{{old('name')}}">
                 {!! $errors->first('name', '<small>:message</small><br>') !!}
-
-        <input type="email" name="email" placeholder="Email..." value="{{ old('email')}}">
+        </div>    
+        <input class="form-control bg-light shadow-sm border-0" type="email" name="email" placeholder="Email..." value="{{ old('email')}}">
                 {!! $errors->first('email', '<small>:message</small><br>') !!}  
 
-        <input name="subject" placeholder="Asunto..." value="{{old('subject')}}">
+        <input class="form-control bg-light shadow-sm border-0" name="subject" placeholder="Asunto..." value="{{old('subject')}}">
                 {!! $errors->first('subject', '<small>:message</small><br>') !!}  
 
-                <textarea name="content" placeholder="Mensaje...">{{old('content')}}</textarea>
+                <textarea class="form-control bg-light shadow-sm border-0" name="content" placeholder="Mensaje...">{{old('content')}}</textarea>
                 {!! $errors->first('content', '<small>:message</small><br>') !!}  
 
-                <button>Enviar</button>
+                <button class="btn btn-primary btn-lg btn-block">Enviar</button>
 
     </form>
-    
+</div>
 @endsection
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,62 +1,63 @@
-<nav class="navbar navbar-expand-md bg-white shadow-sm">
+<nav class="navbar navbar-light navbar-expand-md bg-white shadow-sm">
 
-<a class="navbar-brand" href="{{route('home')}}">
+         <a class="navbar-brand" href="{{route('home')}}">
 
-    {{config('app.name')}}
+        {{config('app.name')}}
 
-    </a>
+        </a>
 
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    
-        <ul class="nav nav-pills">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        
+            <ul class="nav nav-pills">
 
-            <li class="nav-item">
-                <a class="nav-link {{setActive('home')}}" 
-                    href="{{route('home')}}"
-                    >@lang('Home')</a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link {{setActive('random.*')}}"
-                    href="{{route('random.index')}}"
-                    >@lang('Random')</a></li>
-            <li class="nav-item">
-                <a class="nav-link {{setActive('fee')}}" 
-                    href="{{route('fee')}}"
-                    >@lang('Fee')</a></li>
-            <li class="nav-item">
-                <a class="nav-link {{setActive('reglas')}}" 
-                    href="{{route('reglas')}}"
-                    >@lang('Reglas')</a></li>
-            <li class="nav-item">
-                <a class="nav-link {{setActive('tra')}} " 
-                    href="{{route('tra')}}"
-                    >@lang('Traders')</a>
-            </li>
-            @guest
                 <li class="nav-item">
-                    <a class="nav-link" {{setActive('login')}}
-                    href="{{route('login')}}"
-                    >@lang('Login')</a>
-                </li>    
+                    <a class="nav-link {{setActive('home')}}" 
+                        href="{{route('home')}}"
+                        >@lang('Home')</a>
+                </li>
 
-            @else
-            
-            <li class="nav-item">
-                <a class="nav-link" {{setActive('Cerrar Sesion')}} 
-                href="#" onclick="event.preventDefault();
-                document.getElementById('logout-form').submit();">Cerrar Sesion</a></li>
+                <li class="nav-item">
+                    <a class="nav-link {{setActive('random.*')}}"
+                        href="{{route('random.index')}}"
+                        >@lang('Random')</a></li>
+                <li class="nav-item">
+                    <a class="nav-link {{setActive('fee')}}" 
+                        href="{{route('fee')}}"
+                        >@lang('Fee')</a></li>
+                <li class="nav-item">
+                    <a class="nav-link {{setActive('reglas')}}" 
+                        href="{{route('reglas')}}"
+                        >@lang('Reglas')</a></li>
+                <li class="nav-item">
+                    <a class="nav-link {{setActive('tra')}} " 
+                        href="{{route('tra')}}"
+                        >@lang('Traders')</a>
+                </li>
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" {{setActive('login')}}
+                        href="{{route('login')}}"
+                        >@lang('Login')</a>
+                    </li>    
 
-            @endguest
-            
-            
-    
-        </ul>
-    </div>
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link" {{setActive('Cerrar Sesion')}} 
+                        href="#" onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">Cerrar Sesion</a>
+                    </li>                                    
+
+                @endguest
+                
+                
+        
+            </ul>
+        </div>
+
 </nav>
 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
     @csrf
